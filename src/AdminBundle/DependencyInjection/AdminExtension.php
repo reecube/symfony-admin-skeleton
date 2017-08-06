e6972f94cbc75b509e8dddd8c72398c73f9a182a
Yves Riedener <git@reecube.com>
2017-08-06 20:02:00 +0200

    fixed configuration bundle load issue

--- src/AdminBundle/DependencyInjection/AdminExtension.php
+++ src/AdminBundle/DependencyInjection/AdminExtension.php
@@ -2,14 +2,28 @@
 
 namespace AdminBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
 
-class AdminExtension extends Extension
+class AdminExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     */
     public function load(array $configs, ContainerBuilder $container)
+    {
+        // not in use at the moment
+    }
+
+    public function prepend(ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
